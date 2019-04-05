a1976dc9e9fe45591869ccc5d36f2f4aa9930bdb
extrogenesis <extrogenesis@gmail.com>
2019-04-04 21:45:31 -0400

    example of for..of link to MDN

---
+++ Javascript/vanilla/for-loop/for-of.js
@@ -0,0 +1,12 @@
+// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/statements/for...of
+
+// iterate over objects, arrays, strings, set, map...
+
+
+
+let array = [10, 20, 30]
+
+for (let value of array){
+	value += 1
+	console.log(value)
+}
